e1caaed719d1d0fa9871af4b54dc5ae4472430c1
jalenjaloney <jalendeloney@gmail.com>
2026-05-11 17:39:53 -0700

    data loading hanging

--- load_denormalized.sh
+++ load_denormalized.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 file=$1
+
 unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1079/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -15,7 +15,7 @@ files='/data/tweets/geoTwitter21-01-01.zip
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time sh -c 'ls data/* | parallel sh load_denormalized.sh {}'
+echo "$files" | time parallel sh load_denormalized.sh {}
 
 # NOTE:
 # I have removed the pg_normalized code from this repo.
@@ -26,4 +26,4 @@ time sh -c 'ls data/* | parallel sh load_denormalized.sh {}'
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time sh -c 'ls data/* | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3079/postgres --inputs={}'
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3079/ --inputs {}
